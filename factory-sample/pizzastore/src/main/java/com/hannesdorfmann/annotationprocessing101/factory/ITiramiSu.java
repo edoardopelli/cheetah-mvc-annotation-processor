f8741cb07444badd5ac20cdfd9c2952a28b6fc81
Edoardo Pelli <pelli@got.local>
2019-09-27 17:09:06 +0200

    change

---
+++ factory-sample/pizzastore/src/main/java/com/hannesdorfmann/annotationprocessing101/factory/ITiramiSu.java
@@ -0,0 +1,8 @@
+package com.hannesdorfmann.annotationprocessing101.factory;
+
+import org.cheetah.spring.annotations.CheetahSpring;
+
+@CheetahSpring(entity="com.hannesdorfmann.annotationprocessing101.factory.Tiramisu",httprest="/", pkgroot = "org.cheetah.spring")
+public interface ITiramiSu {
+
+}
